033558ca1989a436f0bd6909bfd3eb59ff8d65f3
tassavarat <tassavarat@gmail.com>
2019-03-16 15:39:02 -0700

    Wrote skeleton loops and added count variable to return

--- _printf.c
+++ _printf.c
@@ -9,5 +9,26 @@ int _printf(const char *format, ...)
 		{"i", print_integer},
 		{NULL, NULL}
 	};
-	
+	va_list ap;
+	int i = 0;
+	int j = 0;
+	int count = 0;
+
+	va_start(ap format);
+	while (format && format[i])
+	{
+		while (argument[j].parameter)
+		{
+			if (*argument[j].parameter == format[i])
+			{
+				argument[j].f(ap);
+				++count;
+			}
+			++j;
+		}
+		j = 0;
+		++i;
+	}
+	va_end(ap);
+	return(count);
 }
