eeda0a3dae53c22c1c54725cfc56d256ac62545b
tassavarat <tassavarat@gmail.com>
2019-03-17 17:23:44 -0700

    Rewrote count for integers

--- helper_functions.c
+++ helper_functions.c
@@ -67,20 +67,20 @@ int _print_int(va_list ap)
 	
 	if (n < 0)
 	{
-		_putchar('-');
+		count += _putchar('-');
 	}
-	for (i = 1000000000; i > 0; i /= 10, count++)
+	for (i = 1000000000; i > 0; i /= 10)
 	{
 		if (n / i)
 		{
 			if ((n / i) % 10 < 0)
-				_putchar(-(n / i % 10) + '0');
+				count += _putchar(-(n / i % 10) + '0');
 			else
-				_putchar((n / i % 10) + '0');
+				count += _putchar((n / i % 10) + '0');
 		}
 		else if (n / i == 0 && i == 1)
 		{
-			_putchar(n / i % 10 + '0');
+			count += _putchar(n / i % 10 + '0');
 		}
 	}
 	return (count);
