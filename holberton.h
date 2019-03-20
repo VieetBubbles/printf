cf0783c7f6cfc3cd33f3b8dad35cb3dea21147a7
VieetBubbles <537@holbertonschool.com>
2019-03-20 04:18:49 +0000

    Added hexidemial function to the array

--- holberton.h
+++ holberton.h
@@ -28,6 +28,7 @@ int _print_rot13(va_list ap);
 int _print_unsigned(va_list ap);
 int _print_octal(va_list ap);
 int _print_binary(va_list ap);
+int _print_hex(va_list ap);
 char *convert(unsigned int num, int base);
 int get_print(const char *format, print_type argument[], va_list ap);
 #endif /* _HOLBERTON_H_ */
